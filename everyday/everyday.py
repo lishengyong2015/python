4fe7bd2a00bbb71e5392cfd6fb6249b54e498961
lishengyong <lsy_icloud@icloud.com>
2019-05-09 22:22:54 +0800

    Update everyday.py

--- everyday/everyday.py
+++ everyday/everyday.py
@@ -1,1 +1,39 @@
 #-*- coding: utf-8 -*-
+import math
+import time
+
+def isPrime(num):
+	if num <= 3:
+		return num > 1
+		pass
+	if (num % 6 != 1) and (num % 6 != 5):
+		return False
+		pass
+	sqrts = int(math.sqrt(num))
+	i=5
+	while i <= sqrts:
+		if (num % i == 0) or (num % (i + 2) == 0):
+			return False
+			pass
+		i = i + 6
+		pass
+	return True
+	pass
+
+starttime = time.time()
+cnt=0
+lastprime=0
+while True:
+	curtime = time.time()
+	if curtime - starttime > 10.0:
+		break
+		pass
+	#print(str(curtime-starttime))
+	if isPrime(cnt):
+		lastprime=cnt
+		#print(str(cnt))
+		pass
+	cnt=cnt+1
+	pass
+
+print("lastprime is "+str(lastprime))
