32a46c5eaee736b67c613c22f1f5eb36685384c5
lishengyong <lsy_icloud@icloud.com>
2019-06-02 22:50:18 +0800

    提交udp通信测试

--- socket/clientsocket.py
+++ socket/clientsocket.py
@@ -1,16 +1,12 @@
 #-*- coding: utf-8 -*-
 import socket
 
+#udp test
 s_handle=socket.socket(socket.AF_INET,socket.SOCK_DGRAM,0)
 ip_port=("127.0.0.1", 4096)
-s_handle.bind(ip_port)
-print('start...')
+
 while True:
-	data,address=s_handle.recvfrom(1024)
-	print(data,address)
-	if data=='exit':
-		break
-		pass
-	pass
+	inp=raw_input('please input:')
+	s_handle.sendto(inp, ip_port)
 
 s_handle.close()
